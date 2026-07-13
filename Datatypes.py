3bb491270559565c4a6c9b9469b3580bd5195229
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2026-07-13 14:51:04 +0530

    All datatypes in python

--- Datatypes.py
+++ Datatypes.py
@@ -1,4 +1,9 @@
-#Integers
+#text data type in python - str
+name = "Piyush"
+
+
+#Numeric data types in python - int, float, complex
+#Integers 
 age = 20
 score = -10
 
@@ -6,3 +11,50 @@ score = -10
 price = 10.5
 temperature = -5.5
 pi = 3.14
+
+#Complex
+complex_num = 3 + 4j
+
+
+#Sequence type in python - list, tuple, range
+#List
+fruits = ["apple", "banana", "orange"]
+
+#Tuple
+colors = ("red", "green", "blue")
+
+#Range
+numbers = range(1, 10)
+
+#Mapping type in python - dict
+#Dictionary\
+person = {
+    "name": "Piyush",
+    "age": 20,
+    "city": "New York"
+}
+
+#set data type in python - set
+#Set
+unique_numbers = {1, 2, 3, 4, 5}
+
+
+#Boolean data type in python - bool
+is_student = True
+is_adult = False
+
+
+#Binary data type in python - bytes, bytearray, memoryview
+#Bytes
+byte_data = b"Hello"
+
+#Bytearray
+byte_array_data = bytearray(b"Hello")
+
+#Memoryview
+memory_view_data = memoryview(byte_data)
+
+
+#None data type in python - NoneType
+#None
+none_value = None
