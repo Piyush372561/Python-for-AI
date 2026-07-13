18c89bd51b96d36c94ebf74e61a9076245404248
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2026-07-13 17:28:20 +0530

    String Practice set completed

--- PacticeSet3.py
+++ PacticeSet3.py
@@ -18,3 +18,21 @@ You are selected as a ML intern in Microsoft!
 name = input("Enter your name: ")
 date = input("Enter date: ")
 print(letter.replace("<|Name|>", name).replace("<|Date|>", date))
+
+#Question-3: Write a python program to detect double spaces in a string.
+str = input("Enter a string: ")
+if "  " in str:
+    print("Double spaces detected!")
+else:
+    print("No double spaces detected!")
+
+#Question-4
+print(str.replace("  ", "__"))
+
+
+#Question-5:
+letter = "Hello!,\n This is Piyush.\n I am a ML intern at Microsoft."
+
+print(letter)
+
+
