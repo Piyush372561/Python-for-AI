12aa31e0fc574f48b123d3bddd2ee7c6d21f6523
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2026-07-13 15:36:49 +0530

    Learned how to input, how to square a number, addition, to find the average of two numbers

--- PracticeSet1.py
+++ PracticeSet1.py
@@ -1,5 +1,40 @@
+#
 Num1 = 4
 Num2 = 10
 
 Addition = Num1 + Num2
 print("Addition of two numbers is:", Addition)
+
+
+#Question-2
+Num3 = int(input("Enter number:"))
+z = 2
+
+remainder = Num3 % z
+print("Remainder of the number is:", remainder)
+print(type(Num3))
+
+
+#Question-3
+a = 34
+b = 80
+
+if a > b:
+    print("a is greater than b")
+else:
+    print("b is greater than a")
+
+
+#Question-4
+Num4 = int(input("Enter number 4:"))
+Num5 = int(input("Enter number 5:"))
+
+Average = (Num4 +Num5) / 2
+print("Average of two numbers is:", Average)
+
+
+#Question-5
+Num6 = int(input("Enter number 6:"))
+
+sqr = Num6 ** 2
+print("Square of the number is:", sqr)
