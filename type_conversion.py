50ba915b400b0f0af12090d0a501134327c69312
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2026-07-13 15:19:20 +0530

    Solving Practice set No. 1

---
+++ type_conversion.py
@@ -0,0 +1,31 @@
+num1 = 4
+num2 = 10.5
+num3 = 2j
+
+C1 = float(num1)
+print(C1)
+
+C2 = int(num2)
+print(C2)
+
+C3 = complex(num1)
+print(C3)
+
+C4 = int(num3)
+print(C4)
+
+
+print(type(C1))
+print(type(C2))
+print(type(C3))
+print(type(C4))
+      
+
+
+
+
+
+
+
+
+
