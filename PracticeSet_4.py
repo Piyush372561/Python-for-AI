f89a1430ae9beb2061453062b6ba0afb26e336de
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2026-07-14 17:28:12 +0530

    Completed the Practice sheet 4 learn about the sum() and count() functions

---
+++ PracticeSet_4.py
@@ -0,0 +1,73 @@
+#Question-1
+'''l1 = input("Enter 7
+: ")
+print(l1)
+y = list(l1)
+print(y) 
+
+#Write a program to store seven
+#  in a list entered by the user.
+# 
+Marks = []
+
+m1 = input("Enter marks here: ")
+Marks.append(m1)
+f2 = input("Enter marks here: ")
+Marks.append(f2)
+f3 = input("Enter marks here: ")
+Marks.append(f3)
+f4 = input("Enter marks here: ")
+Marks.append(f4)
+f5 = input("Enter marks here: ")
+Marks.append(f5)
+f6 = input("Enter marks here: ")
+Marks.append(f6)
+f7 = input("Enter marks here: ")
+Marks.append(f7)
+
+print(Marks)
+
+
+#Question-2
+
+Marks = []
+
+m1 = int(input("Enter marks here: "))
+Marks.append(m1)
+m2 = int(input("Enter marks here: "))
+Marks.append(m2)
+m3 = int(input("Enter marks here: "))
+Marks.append(m3)
+m4 = int(input("Enter marks here: "))
+Marks.append(m4)
+m5 = int(input("Enter marks here: "))
+Marks.append(m5)
+m6 = int(input("Enter marks here: "))
+Marks.append(m6)
+m7 = int(input("Enter marks here: "))
+Marks.append(m7)
+
+Marks.sort()
+print(Marks)
+ 
+
+#Question-3
+
+tp = (2,3,4,5,6)
+
+tp[0] = "Piyush"
+print(tp)
+'''
+
+#Question-4
+l = [1,2,3,4,5]
+
+print(sum(l)) #Built-in sum() to add values of a list or tuple
+tp = (1,2,3,4,5,6,7)
+print(sum(tp))
+
+#Question-5
+a = (7,0,8,0,0,9)
+c = a.count(0)
+print(c)
+
