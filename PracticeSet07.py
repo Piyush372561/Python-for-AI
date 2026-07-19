21a421dcb9c77b0433cb9343512ae449fbcc1d1f
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2026-07-19 15:25:40 +0530

    Practice Sheet 07

--- PracticeSet07.py
+++ PracticeSet07.py
@@ -1,5 +1,5 @@
 #Question-1
-'''
+
 n = int(input("n : "))
 i = int(input("i : "))
 print("Table of :", n)
@@ -10,7 +10,7 @@ while i <= 10:
      print(table)
 
 print("Done")
-'''
+
 #Question-2: Write a program to greet all the person names stored in a list ‘lʼ and which starts with S.
 l = ["Harry", "Soham", "Sachin", "Rahul"]
 i = 0
@@ -23,7 +23,7 @@ while i<len(l):
     
 print("Finished.")
 
-#Question-3: 
+#Question-3: Multiplication of a table using for loop.
 
 
 
@@ -36,12 +36,43 @@ print("Finished.")
 
 
 
-#Question-4: Check a number is prime or not.
+#Question-4: Check a number is prime or not.(yeh humne likha tha)
 num = int(input("Enter number: "))
 
 for i in range(2, num):
    if(num % i == 0):
       print("Prime number.")
-      
+      break  
 else:
    print("Composite number.")
+
+
+#Question-4: 
+num = int(input("Enter number: "))
+is_prime = "True"
+
+for i in range(2, num):
+   if num % i == 0:
+    is_prime = False
+    break
+
+
+if is_prime:
+   print(num,"is a Prime number")
+else:
+   print(num,"is a Composite number.")
+
+
+#Question-5:
+p = int(input("Enter number: "))
+i = int(input("Enter number: "))
+sum = 0
+
+while i <= p:
+   sum = i + sum
+   i = i + 1
+
+print("Sum of first N natural numbers: ",sum)
+
+
+   
