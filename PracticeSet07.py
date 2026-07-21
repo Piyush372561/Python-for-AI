f2406de50732a051c430020be6f6659a8d3f07c2
Piyush kushwaha <kushwahapiyush2004@gmail.com>
2026-07-21 16:52:49 +0530

    Practice set 07 Done

--- PracticeSet07.py
+++ PracticeSet07.py
@@ -1,5 +1,5 @@
 #Question-1
-
+'''
 n = int(input("n : "))
 i = int(input("i : "))
 print("Table of :", n)
@@ -22,18 +22,6 @@ while i<len(l):
      i = i + 1
     
 print("Finished.")
-
-#Question-3: Multiplication of a table using for loop.
-
-
-
-
-
-
-
-
-
-
 
 
 #Question-4: Check a number is prime or not.(yeh humne likha tha)
@@ -75,4 +63,54 @@ while i <= p:
 print("Sum of first N natural numbers: ",sum)
 
 
-   
+#Question-6: Question-1 using for loop
+n = int(input("n : "))
+print("Table of :", n)
+
+
+for i in range(1, 11):
+    table = n * i
+    print(table)
+    
+#Question-7: Factorial program
+n = int(input("N : "))
+factorial = 1
+for i in range(n,0,-1):
+    factorial = factorial * i
+
+print(factorial)
+
+
+#Question-8:
+#Write a program to print the following star pattern:
+for r in range(4):
+    for c in range(2 * r - 1):
+        print("*", end=" ")
+
+    print()
+
+#Question-9:
+for i in range(4):
+    for j in range(i):
+        print("*", end="")
+    print()
+
+
+#Question-10:
+for r in range(1,4):
+    if(r % 2 == 0):
+        for c in range(1,3):
+            print("*", end="")
+    else:
+        for c in range(1,4):
+            print("*", end="")
+
+    print()
+   '''
+    
+#Question-11: Write a program to print multiplication table of n using for loops in reversed order
+n = int(input("n = "))
+
+for i in range(10, 0, -1):
+    table = n * i
+    print(table)
